b603b44b2d0089fb341d17a76d49dfe785658cec
Remmy Clay <remmy.lauren@gmail.com>
2017-11-06 21:55:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,8 +6,26 @@ def display_board(current_board)
     puts " #{current_board[6]} | #{current_board[7]} | #{current_board[8]} "
 end
 
-def valid_move
-  position_taken
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0, 8)
+    return true
+  elsif position_taken?(board, index)
+    return false
+  elsif !index.between?(0, 8)
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if (board[index] == "X" || board[index] == "O")
+    return true
+  elsif (board[index] != " " || board[index] != "" || board[index] != nil)
+    return false
+  end
 end
 
 def move(board, index)
