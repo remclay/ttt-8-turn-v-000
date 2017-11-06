59bdd81bb9a6778852b7e0a5164b0dd6117aee32
Remmy Clay <remmy.lauren@gmail.com>
2017-11-06 21:33:06 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+def display_board(current_board)
+    puts " #{current_board[0]} | #{current_board[1]} | #{current_board[2]} "
+    puts "-----------"
+    puts " #{current_board[3]} | #{current_board[4]} | #{current_board[5]} "
+    puts "-----------"
+    puts " #{current_board[6]} | #{current_board[7]} | #{current_board[8]} "
+end
+
+def valid_move
+  position_taken
+end
+
+def move(board, index)
